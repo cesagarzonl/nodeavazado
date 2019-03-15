bfd897f5419298ef5aa914d402ef9d0e41f6c90f
cesagarzonl <cigarzon8@misena.edu.co>
2019-03-15 17:16:36 -0500

    se genera toda la conectivida de la base de datos

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -1,7 +1,22 @@
 'use strict'
 
+const setupDatabase = require('./lib/db')
+const setupAgenteModel = require('./model/agente')
+const setupModelsModel = require('./model/metric')
+
 module.exports = async function (configuracion) {
-  const Agent = {	}
+  const sequelize = setupDatabase(configuracion)
+  const AgenteModel = setupAgenteModel(configuracion)
+  const MetricsModel = setupModelsModel(configuracion)
+
+  AgenteModel.hasMany(MetricsModel)
+  MetricsModel.belongsTo(AgenteModel)
+
+  await sequelize.authentiaate()
+
+  sequelize.sync()
+
+  const Agent = {}
   const Metric = {}
   return {
     Agent, Metric
