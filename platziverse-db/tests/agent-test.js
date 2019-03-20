c87e253b42e724dfc5dbe0afcbdbe7515e0f8041
cesagarzonl <cigarzon8@misena.edu.co>
2019-03-20 11:17:48 -0500

    se hacen pruebas del modulo con ava nyc

--- platziverse-db/tests/agent-test.js
+++ platziverse-db/tests/agent-test.js
@@ -1,18 +1,42 @@
 'use strict'
 
 const test = require('ava')
-
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
+const agentFixtures = require('./fixtures/agent')
 let config = {
   logging: function () {}
 }
 
-let db = null
 
+let MetricStub = {
+  belongsTo:sinon.spy()
+}
+let single = Object.assign({},agentFixtures.single)
+let AgentStub = null
+let db = null
+let sandbox =null
 test.beforeEach(async () => {
-  const setupDatabase = require('../')
+	sandbox = sinon.sandbox.create()
+  AgentStub ={
+  	hasMany:sandbox.spy()
+  }
+
+  const setupDatabase = proxyquire('../',{
+  	'./models/agente':() =>AgentStub,
+  	'./models/metric':() =>MetricStub
+  })
   db = await setupDatabase(config)
 })
-
+test.afterEach( ()=> {
+	sandbox && sinon.sandbox.restore()
+})
 test('Agent', t => {
   t.truthy(db.Agent, 'Agent Exist')
+})
+test.serial('Setup', t =>{
+	t.true(AgentStub.hasMany.called,'AgenntModel.hasMany was execute')
+	t.true(AgentStub.hasMany.calledWith(MetricStub),'Agument needs to be the model')
+	t.true(MetricStub.belongsTo.calledWith(AgentStub),'Agument needs to be the model')
+	t.true(MetricStub.belongsTo.called,'MetricModel.hasMany was execute')
 })
---
+++ platziverse-db/tests/fixtures/agent.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const agent ={
+	id: 1,
+	uuid: "yyy-yyy-yyy",
+	name:'fixture',
+	username:'plarzi',
+	hostname:'test-host',
+	pid:0,
+	connected:true,
+	createdAd:new Date(),
+	updatedAd: new Date()
+}
+
+const agents =[
+	agent,
+	extender(agent, { id: 2, uuid:'yyy-yyy-yyw',connected: false, username:"platzy2"}),
+	extender(agent, { id: 3, uuid:'yyy-yyy-yyx'}),
+	extender(agent, { id: 4, uuid:'yyy-yyy-yyz',username:"platzy2"})
+]
+
+function extender (obj, values){
+	const clone = Object.assign({},obj)
+	return Object.assign(clone,values)
+}
+module.exports ={
+	single:agent,
+	all:agents,
+	connected:agents.filter(a=>a.connected),
+	plartzi:agents.filter(a=>a.username === 'plarzi'),
+	byUuid:id => agents.filter(a=>a.uuid === id).shift(),
+	byId:id => agents.filter(a=>a.id === id).shift()
+}
